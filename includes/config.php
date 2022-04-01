7dce12b4c9402c03e57f7f06808820ec4e57244e
hokole <hokole551@gmail.com>
2022-04-01 01:08:56 -0500

    update config

--- includes/config.php
+++ includes/config.php
@@ -1,10 +1,10 @@
 <?php
 $url = parse_url(getenv("mysql://bde1900971353c:2b4ac360@us-cdbr-east-05.cleardb.net/heroku_d347ebb2241490f?reconnect=true"));
 
-$server = $url["us-cdbr-east-05.cleardb.net"];
-$username = $url["bde1900971353c"];
-$password = $url["2b4ac360"];
-$db = substr($url["heroku_d347ebb2241490f"], 1);
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
 
 $conn = new mysqli($server, $username, $password, $db);
 ?>
