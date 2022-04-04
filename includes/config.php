f39640e65d5df1a9487157696a056d4dd244ab25
hokole <hokole551@gmail.com>
2022-04-03 22:31:08 -0500

    config

--- includes/config.php
+++ includes/config.php
@@ -1,5 +1,5 @@
 <?php
-$url = parse_url(getenv("mysql://bde1900971353c:2b4ac360@us-cdbr-east-05.cleardb.net/heroku_d347ebb2241490f?reconnect=true"));
+$url = parse_url("mysql://bde1900971353c:2b4ac360@us-cdbr-east-05.cleardb.net/heroku_d347ebb2241490f?reconnect=true");
 
 $server = $url["host"];
 $username = $url["user"];
